e329f3ad4caddac062d4f384ef0d96836018417d
Advaith <Advaith.Apparasu865@education.nsw.gov.au>
2026-06-21 11:59:12 +1000

    "activity done"

---
+++ musicplaylistmanager.py/a1.py
@@ -0,0 +1,27 @@
+class Playlist:
+    def __init__(self, name, genre):
+        self.name, self,genre, self.songs - name, genre []
+        print(f"{name} ({genre}) ready!")
+
+    def add_song(self, song):
+       self.songs.append(song)
+
+    def remove_song(self, song):
+        if song in self.songs:
+            self.songs.remove(song)
+
+    def display(self)
+        print(f"/n{self.name}({self.genre})")
+        print("self.songs, sep ="/n") if self.songs else print ("No songs")
+              
+    def_del_(self):
+        print(f"{self.name} deleted!")
+
+p = Playlist("Road Trip Mix", "Pop")
+
+While True:
+    C = input("\n1.add" 2.Remove 3.View 4.Quit:")
+    
+
+
+    
