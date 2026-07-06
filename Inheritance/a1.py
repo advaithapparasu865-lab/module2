2a5baeaba11c9aec36395a869aceef7632894888
Advaith <Advaith.Apparasu865@education.nsw.gov.au>
2026-07-06 11:47:46 +1000

    class instrument

--- Inheritance/a1.py
+++ Inheritance/a1.py
@@ -4,7 +4,7 @@ class Animal:
        self.habitat =  habitat
     
 class Dog(Animal):
-    def__init__(self, nmae, habitat, breed):
+    def__init__(self, name, habitat, breed):
        Animal.__init__(self, name, habitat)
        self.breed = breed
     
